6cfabee0bedd551eee545807cf6bb42a9e2e427d
ok2ju <soulSpringg@gmail.com>
2014-11-09 12:35:47 +0200

    added ordered select

--- select.sql
+++ select.sql
@@ -30,6 +30,65 @@ on Student.id_address = Address.id_address;
 select * from
 Student, Address;
 
+/* -------- UNION -------------------- */
+(select mark, semester from Marks where semester = 1)
+union
+(select mark, semester from Marks where mark = 9);
+
+/* --------- UNION ALL --------------- */
+(select mark, semester from Marks where semester = 1)
+union all
+(select mark, semester from Marks where mark = 9);
+
+/* --------- INTERSECT ---------------- */
+/* возвращает точное совпадение подмножеств t1 и t2
+проще говоря одинаковые строки, которые есть в t1 и t2 */
+select * from t2 intersect select * from t1
+
+/* --------- EXCEPT (MINUS) ------------ */
+/* MINUS как бы вычитает из множества А множество Б и
+возвращает уникальные строки, которые не встречаются в множестве Б */
+select * from t2 minus select * from t1
+
+/* --------- GROUP, HAVING ------------ */
+/* выдает информацию об учениках, у которых средний(годовой) балл
+по определенному предмету выше 8 и сортирует записи по имени ученика */
+select s.person.name, s.person.surname, AVG(mark), semester, title
+from Student s, Marks, Subject
+where Marks.id_subject = Subject.id_subject and
+Marks.id_student = s.id_student and
+Subject.title = 'История'
+group by s.person.name, s.person.surname, semester, title
+having AVG(mark) > 8
+order by s.person.name;
+
+/* ----------- MERGE EXAMPLE ------------------ */
+create table bonus (
+id_address int,
+bonus number default 100);
+
+insert into bonus(id_address) values (1);
+insert into bonus(id_address) values (2);
+insert into bonus(id_address) values (4);
+insert into bonus(id_address) values (6);
+insert into bonus(id_address) values (7);
+
+merge into bonus b
+using (
+  select id_address, street, state
+  from Address
+  where state = 'Беларусь') e
+on (b.id_address = e.id_address)
+when matched then
+  update set b.bonus = e.id_address + 1
+  delete where (e.id_address < 2)
+when not matched then
+  insert (b.id_address, b.bonus)
+  values (e.id_address, e.id_address * 0.05)
+  where (e.id_address > 2);
+
+select * from bonus;
+
 /* --------- Информация об учениках ---------- */
 select s.person.name, s.person.surname, dateEnrollment, street,
 city, state, class_number, class_character
