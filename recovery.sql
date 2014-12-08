a722cd2780d432673c8c422dc53038b3124e957f
ok2ju <soulSpringg@gmail.com>
2014-12-08 15:30:39 +0200

    update recovery.sql

--- recovery.sql
+++ recovery.sql
@@ -3,3 +3,10 @@ SHUTDOWN IMMEDIATE
 STARTUP MOUNT
 ALTER DATABASE ARCHIVELOG;
 ALTER DATABASE OPEN;
+
+set oracle_sid=orcl
+echo %oracle_sid%
+rman target /
+backup database;
+list backup;
+restore database;
